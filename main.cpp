c3b3ab5f70098252c30be3d484c9cf0603c06345
Jimmy <Jimmy090804@gmail.com>
2016-03-03 20:07:44 -0600

    Added main.cpp
    Just an empty c++ file for us to work on.

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ * Date Modified: 3/3/2016
+ * Team members: Ho Ting Jimmy Yeung, Asad Waheed, Chad Smith
+ *
+ *
+ *
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // TODO:
+}
