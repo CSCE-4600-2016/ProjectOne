41266cf90c479652470590e0a7ae2b86315a94ba
Asad Waheed <asad.waheed.23@gmail.com>
2016-03-10 15:40:10 -0600

    more formatting

--- Scheduler.h
+++ Scheduler.h
@@ -1,6 +1,6 @@
 // File: Scheduler.h
-// Description: A base class sechduler that all the algoorithms
-// can derive from as subclasses
+// Description: A base class sechduler that all the algorithms can derive from as subclasses
+// Adding more headers for the two that are commented out right now 
 
 // #include "ProcessTable.h"
 // #include "ProcessAlgorithms.h"
@@ -13,11 +13,19 @@ class Scheduler
 {
 	private:
 
+		// Data/variables
+		ProcessTable processTable;
+		SchedulingAlgorithm *algorithm;
+		SchedAlgoParams algoParams;
+		std::std::vector<PCB> readyProcesses;
+		//Functions
+		SchedulingAlgorithm *CreateAlgorithm(const std::string &AlgorithmName);
+		
+
 	public:
 
 		/* Takes the algo name, and required params*/
-		Scheduler(const std::string &AlgorithmName, int csp,
-					int quantum = 0, int m = 0);
+		Scheduler(const std::string &AlgorithmName, int csp, int quantum = 0, int m = 0);
 		~Scheduler();	
 
 		/* LoadFile method, if we are taking in processes from a file */
@@ -27,9 +35,11 @@ class Scheduler
 		void RunProcesses();
 
 		/* Getter for the ProcessTable */
-		/* ProcessTable *GetProcessTable(){ return }*/
+		/* ProcessTable *GetProcessTable(){ return &processTable } */
+
+		/* Getter for the algorithm paramters */
+		/* SchedAlgoParams *GetSchedAlgoParams(){ return &algoParams }*/
+
 };
-
-
 
 #endif // SCHEDULER_H
