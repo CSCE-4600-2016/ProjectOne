78cf45d59f1cea95e2a904ed682ce9d2ae1a42b7
Jimmy <Jimmy090804@gmail.com>
2016-03-22 19:43:00 -0500

    Finished QuadCore SJF

--- src/ProcessSimulator.cpp
+++ src/ProcessSimulator.cpp
@@ -40,12 +40,10 @@ void ProcessSimulator::RunSimulation()
 		// if we are in multiProcess mode then run a quad core simulation
 		multiProcessMode ? RunFifoQuadCoreSimulation() : RunFifoSimulation();
 		break;
-	case SRT:
-		break;
 	case SJF:
-		RunSJFSimulation();
+        multiProcessMode? RunSJFQuadCoreSimulation() : RunSJFSimulation();
 		break;
-	case LRT: break;
+
 	default: break;
 	}
 }
@@ -281,150 +279,85 @@ void ProcessSimulator::RunSJFSimulation()
 
 }
 
+void ProcessSimulator::RunSJFQuadCoreSimulation()
+{
+    vector<Process> processSet;
+    
+    // Pass the processes in queue into an array. This makes the whole thing easier.
+    while (scheduledProcesses.GetNumberProcesses() > 0)
+    {
+        Process newProcess = scheduledProcesses.FirstProcess();
+        processSet.push_back(newProcess);
+        scheduledProcesses.PopProcess();
+    }
+    
+    // Sort the vector so we always get SJ Process
+    sort(processSet.begin(), processSet.end());
+    
+    // how many processors we currently have left to run a process
+    unsigned int processorsLeft = 4;
+    unsigned int totalCycles = 0;
+    
+    // all the processes that are running right now
+    std::vector<Process> runningProcesses;
+    
+    // start with one running process
+    runningProcesses.push_back(processSet.at(0));
+    processSet.erase(processSet.begin() + 0);
+    
+    
+    // for a multiprocess system we are going to treat each time around the loop as one cycle. This way each we always know when to add or remove a process
+    while (processSet.size() > 0)
+    {
+        // the process that is scheduled next to run
+        Process waitingProcess = processSet.at(0);
+        // if we have an open processor AND we have a process waiting then we give this process to some processor
+        if (processorsLeft > 0 && totalCycles >= waitingProcess.arrivalTime)
+        {
+            // asign this process to some processor so it can do some work
+            processorsLeft--;
+            runningProcesses.push_back(waitingProcess);
+            // we need to calculate the waiting time that this process had to wait for an open processor
+            waitingTime += (totalCycles - waitingProcess.arrivalTime);
+            
+            // we now should have one less process scheduled
+            processSet.erase(processSet.begin() + 0);
+            
+        }
+        // either we don't have a processor waiting or their isn't a process that has arrived
+        else
+        {
+            // loop through all the processes that are currently running on all the processors
+            for (unsigned int i = 0; i < runningProcesses.size(); i++)
+            {
+                // do we have a process that has finished up?
+                if (runningProcesses[i].numberCycles <= 0)
+                {
+                    // make a processor avaliable
+                    processorsLeft++;
+                    
+                    // context penalty since processor has to do some work to get ready for next process
+                    totalPenalty += contextPenalty;
+                    
+                    // we are finished with this proces remove it from the set
+                    runningProcesses.erase(runningProcesses.begin() + i);
+                }
+                // process is not done yet, just keep subtracting the number cycles it has left
+                else
+                {
+                    runningProcesses[i].numberCycles--;
+                }
+            }
+        }
+        
+        totalCycles++;
+    }
+}
+
+
 // Round Robin
 void ProcessSimulator::RunRRSimulation()
 {
-	   // Quantum time of 50 cycles, should be fixed
-	   const int quantumTime = 50;
-	   int time = 0;
-	   int isProcessDone = 0; // should be a bool...
-	   
-	   // set remaining procs to number of procs currently in queue
-       int remainProcess = scheduledProcesses.GetNumberProcesses();
-	   // total becomes remaining
-       int totalNumberOfProcess = remainProcess; 
-	   
-       // create a new process array object called processSet
-       // populate with the total number of procs
-	   Process *processSet = new Process[totalNumberOfProcess];
-	   // Populate remaining time, finish or "complete" processes, and waiting processes 
-       // with total # of procs
-       int remainingTime[totalNumberOfProcess];
-	   int finish[totalNumberOfProcess];
-	   int wait[totalNumberOfProcess];
-	   
-       // init counter var and cycle time to 0. Assuming that totCycletime is
-       // going to be used in updating the CSP when this algo runs every iteration
-       int count = 0;
-	   int totalCycleTime = 0;
-	
-	   // Pass the processes in queue into an array. This makes the whole thing easier.
-       // Check while there's still scheduled procs in the queue...
-	   while(scheduledProcesses.GetNumberProcesses() > 0)
-	   {
-           // create new process object and feed it the first scheduled proc 
-	       Process newProcess = scheduledProcesses.FirstProcess();
-          
-           // update process set with counter for index, and set = to above object
-	       processSet[count] = newProcess;
-          
-           // increment totalCycletime by # cycles of the newProcess object
-	       totalCycleTime += newProcess.numberCycles;
-	      
-           // rem. time array gets updated with counter for index, set
-           // equal to the # of cycles 
-	       remainingTime[count] = newProcess.numberCycles;
-	       
-	       // finish is a array keeping track of complete procs. We populate with count and set
-           // == to zero 
-	       finish[count] = 0;
-	       
-           //update count 
-           count++;
-	       
-           // update contextSwitchPenalty
-           totalPenalty += contextPenalty;
-        
-
-           // decrement process from sched queue
-           scheduledProcesses.PopProcess();
-	   }
-
-// 	//-----------------------------------------------------------
-// 	   for(int i = 0; remainProcess != 0;)
-// 	   {
-//             // check if the rem time for the current proc is < quantum
-// 	       if((remainingTime[i] <= quantumTime) && (remainingTime[i] > 0))
-// 	       {
-
-// 	           time += remainingTime[i];
-// 	           remainingTime[i] = 0;
-// 	           isProcessDone = 1;
-// 	       }
-// 	       else if(remainingTime[i] > 0)
-// 	       {
-// 	           remainingTime[i] -= quantumTime;
-// 	           time += quantumTime;
-// 	       }
-	       
-// 	       // If rem time for the current process is zero, 
-//            // and process is done
-// 	       if((remainingTime[i] == 0) && (isProcessDone == 1)) 
-//            {
-// 	           remainProcess--;
-// 	        //   std::cout << time - processSet[i].arrivalTime - processSet[i].numberCycles << std::endl;
-// 	           waitingTime += (time - processSet[i].arrivalTime - processSet[i].numberCycles);
-// 	           isProcessDone = 0;
-// 	       }
-	       
-// 	       if(i == (totalNumberOfProcess - 1))
-// 	       {
-// 	           i = 0;
-// 	       }
-	   
-//            else if (processSet[i+1].arrivalTime <= time)
-// 	       {
-// 	           i++;
-// 	       }
-	   
-//            else
-// 	       {
-// 	           i = 0;
-// 	       }
-// 	   }
-// //---------------------------------------------------
-	   // we set our proc time to 0
-	   int processingTime = 0;
-
-	   for(time = 0; time < totalCycleTime;) // traverse from 0 to totCycleTime
-	   {
-           // as long as we are less than all procs 
-	       for(int i = 0; i < totalNumberOfProcess;i++) 
-	       {
-               // 
-	           if((processSet[i].arrivalTime <= time) && (finish[i] == 0))
-	           {
-	               if(remainingTime[i] < quantumTime)
-	               {
-	                   processingTime = remainingTime[i];
-	               }
-	               else
-	               {
-	                   processingTime = quantumTime;
-	               }
-	               
-	               remainingTime[i] = remainingTime[i] - processingTime;
-	               
-                   if(remainingTime[i] == 0) 
-                   {
-	                   finish[i] = time; // (c) http://stackoverflow.com/questions/14912813/round-robin-scheduling-program
-	               }
-	               for (int j = 0; j < totalNumberOfProcess; j++) 
-                   {
-                       // check for our inner counter != outer counter
-                       // and if unfinished and if the arrival time is
-                       // less than or equal to the current time 
-	                   if(j != i && finish[j] == 0 && processSet[j].arrivalTime <= time)
-	                   {
-                           // Update the waiting time  
-	                       waitingTime += processingTime;
-	                   }
-	               }
-	               time = time + processingTime;
-	           }
-	       }
-	   }
-	   
 }
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -9,33 +9,54 @@
 
 int main(int argc, char** argv)
 {
+    
 	// set cout locale to use the system locale so we can get numbers formatted since
 	std::cout.imbue(std::locale(""));
 	ProcessSet process_set;
+    
+    /*
+     *  FIFO
+     */
 	process_set.BuildProcessSet(std::cin);
+    
 	// build a simulator for a fifo scheduler, with a context switch penalty of 10
 	ProcessSimulator simulator(SchedulingAlgorithim::FIFO, 10, process_set);
-
-
+    
 	std::cout << "Fifo Single Core: Total Processes: " << process_set.GetNumberProcesses() << std::endl;
 	simulator.RunSimulation();
 	
 	std::cout << "Fifo Single Core: Average Wait Time: " << simulator.GetAverageWaitTime() << std::endl;
 	std::cout << "Fifo Single Core: Total Penalty For Context Switching: " << simulator.GetTotalPenalty() << std::endl;
+    
+    // build a simulator for a multiprocessor fifo scheduler, with a context switch penalty of 10
+    ProcessSimulator multiFifo(SchedulingAlgorithim::FIFO, 10, process_set, true);
+    std::cout << "\n\nFiFo Quad Core: Total Processes: " << process_set.GetNumberProcesses() << std::endl;
+    multiFifo.RunSimulation();
+    
+    std::cout << "FiFo Quad Core: Average Wait Time: " << multiFifo.GetAverageWaitTime() << std::endl;
+    std::cout << "Fifo Quad Core: Total Penalty For Context Switching: " << multiFifo.GetTotalPenalty() << std::endl;
 	
+
 //    // RR
     process_set.BuildProcessSet(std::cin);
+
 //    // build a simulator for a fifo scheduler, with a context switch penalty of 10
     ProcessSimulator simulatorRR(SchedulingAlgorithim::RoundRobin, 10, process_set);
 //    
+
     std::cout << "\nRound Robin: Total Processes: " << process_set.GetNumberProcesses() << std::endl;
     simulatorRR.RunSimulation();
 //    
     std::cout << "Round Robin: Average Wait Time: " << simulatorRR.GetAverageWaitTime() << std::endl;
     std::cout << "Round Robin: Total Penalty For Context Switching: " << simulatorRR.GetTotalPenalty() << std::endl;
+
 	
+    /*
+     *  SJF
+     */
 	process_set.BuildProcessSet(std::cin);
-	// build a simulator for a sjf scheduler, with a context switch penalty of 10
+    
+	// build a simulator for a SJF scheduler, with a context switch penalty of 10
 	ProcessSimulator simulatorSJF(SchedulingAlgorithim::SJF, 10, process_set);
 	
 	std::cout << "\n\nSJF Single Core: Total Processes: " << process_set.GetNumberProcesses() << std::endl;
@@ -43,15 +64,18 @@ int main(int argc, char** argv)
 	
 	std::cout << "SFJ Single Core: Average Wait Time: " << simulatorSJF.GetAverageWaitTime() << std::endl;
 	std::cout << "SJF Single Core: Total Penalty For Context Switching: " << simulatorSJF.GetTotalPenalty() << std::endl;
+    
+    // build a simulator for a multiprocessor SJF scheduler, with a context switch penalty of 10
+    ProcessSimulator multiSJF(SchedulingAlgorithim::SJF, 10, process_set, true);
+    std::cout << "\n\nSJF Quad Core: Total Processes: " << process_set.GetNumberProcesses() << std::endl;
+    multiSJF.RunSimulation();
+    
+    std::cout << "SJF Quad Core: Average Wait Time: " << multiSJF.GetAverageWaitTime() << std::endl;
+    std::cout << "SJF Quad Core: Total Penalty For Context Switching: " << multiSJF.GetTotalPenalty() << std::endl;
 
 
-	// build a simulator for a multiprocessor fifo scheduler, with a context switch penalty of 10
-	ProcessSimulator multiFifo(SchedulingAlgorithim::FIFO, 10, process_set, true);
-	std::cout << "\n\nFiFo Quad Core: Total Processes: " << process_set.GetNumberProcesses() << std::endl;
-	multiFifo.RunSimulation();
 
-	std::cout << "FiFo Quad Core: Average Wait Time: " << multiFifo.GetAverageWaitTime() << std::endl;
-	std::cout << "Fifo Quad Core: Total Penalty For Context Switching: " << multiFifo.GetTotalPenalty() << std::endl;
+
 
 	return 0;
 }
